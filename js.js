bc8f2c6b86ef454279af4c7b41260776c4325ef9
Andrey <88719652+andreyquerino@users.noreply.github.com>
2022-01-06 15:52:53 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,12 +1,11 @@
 // @andreyquerino
 //
-// Detecção da locatização do visitante no Website
+// Localização do usuário através do website
 
-var request = new XMLHttpRequest();
-request.open("GET", "https://wtfismyip.com/json", !0),
-    (request.onload = function () {
-        var y;
-        200 <= this.status && this.status < 400 && ((e = JSON.parse(this.response).YourFuckingLocation.replace(/\,.+/g, "$'")), (document.getElementById("cidade").innerHTML = y));
-    }),
-    (request.onerror = function () {}),
-    request.send();
+async function localizacaoUsuario(){
+    let requisicao = await fetch('https://wtfismyip.com/json');
+    let json = await requisicao.json();
+    let localizacao = json.YourFuckingLocation.split(",");
+    document.getElementById("cidade").innerHTML = localizacao[0];
+}
+localizacaoUsuario();
